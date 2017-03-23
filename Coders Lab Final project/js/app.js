949465c2d43c5d2f7b04d80473fd8c5879f8683d
krztom <tomecki.k@gmail.com>
2017-03-23 23:53:19 +0100

    commit

--- Coders Lab Final project/js/app.js
+++ Coders Lab Final project/js/app.js
@@ -18,5 +18,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     $('#greeting').append(themessage);
 
+    var header = $('#section1');
+    var range = 200;
 
+    $(window).on('scroll', function () {
+
+        var scrollTop = $(this).scrollTop();
+        var offset = header.offset().top;
+        var height = header.outerHeight();
+        offset = offset + height / 2;
+        var calc = 1 - (scrollTop - offset + range) / range;
+
+        header.css({ 'opacity': calc });
+
+        if ( calc > '1' ) {
+            header.css({ 'opacity': 1 });
+        } else if ( calc < '0' ) {
+            header.css({ 'opacity': 0 });
+        }
+
+    });
 });
